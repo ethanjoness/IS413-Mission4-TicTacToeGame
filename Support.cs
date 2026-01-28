9d6e53521ae2abad9c2e0ab08f2b74bf57b07031
micdai000 <daimbd000@gmail.com>
2026-01-28 14:44:09 -0700

    Outline of what to do next for the Support.cs file

--- Support.cs
+++ Support.cs
@@ -2,5 +2,23 @@ namespace Mission4;
 
 public class Support
 {
+    // What do you need to store here as class-level variables?
+    // - The game board state?
+    // - Whose turn is it?
     
+    public void PrintBoard(int[] move)
+    {
+        // 1. Update your internal board with the new move
+        // 2. Figure out if this is X or O
+        // 3. Print the entire board
+    }
+
+
+    public string GameResult(int[] board)
+    {
+        if (board[0] == board[1] && board[1] == board[2])
+        {
+            Console.WriteLine($"Player {board[0]} Won!");
+        }
+    }
 }
--- obj/Debug/net10.0/Mission4.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mission4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b853e6591af0e2e08a117fe8c3a691b738b7c83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
